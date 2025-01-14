81b0e4f5586950299582a67789ba7767a0248174
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-14 15:06:06 +0530

    Add files via upload

---
+++ Visual_studio_leetcode/queue_reconstructor.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reconstructQueue(self, people):
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        ans = []
+        for p in people:
+            ans.insert(p[1], p)
+        return ans
+    
+    
+    
+    
+a = Solution()
+    
+result = a.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]])
+print(result)
