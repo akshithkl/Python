cf4c813f012d7552bed4184a67df378d00ab1df8
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-31 19:15:50 +0530

    Delete Advanced_python directory

--- Advanced_python/design_2.py
+++
@@ -1,6 +0,0 @@
-def right_angled_triangle(rows):
-    for i in range(1, rows + 1):
-        print("*" * i)
-
-rows = int(input("Enter number of rows: "))
-right_angled_triangle(rows)
--- Advanced_python/design_3.py
+++
@@ -1,6 +0,0 @@
-def inverted_triangle(rows):
-    for i in range(rows, 0, -1):
-        print("*" * i)
-
-rows = int(input("Enter number of rows: "))
-inverted_triangle(rows)
