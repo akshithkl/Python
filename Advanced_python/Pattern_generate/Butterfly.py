a3478342173b39cc33fe256d7dd7f28978154cf1
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-02-02 19:32:26 +0530

    Delete Advanced_python/Pattern_generate/Butterfly.py

--- Advanced_python/Pattern_generate/Butterfly.py
+++
@@ -1,8 +0,0 @@
-def butterfly(rows):
-    for i in range(1, rows + 1):
-        print("*" * i + " " * (2 * (rows - i)) + "*" * i)
-    for i in range(rows, 0, -1):
-        print("*" * i + " " * (2 * (rows - i)) + "*" * i)
-
-rows = 4
-butterfly(rows)
